6bd96fe1bc156acfc9b890479ab8a0ba86f0e3e5
Ankit-Saha-2003 <92501201+Ankit-Saha-2003@users.noreply.github.com>
2023-02-12 22:45:21 +0530

    Change database name and add DDL file to this branch

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # Connect to the database
-app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@localhost:5432/stackdb"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@localhost:5432/cqadb"
 db = SQLAlchemy(app)
 # migrate = Migrate(app, db)
 
